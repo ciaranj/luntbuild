d527e499e83e3936872b8f2b96916ea7deaa0e18
lubosp <lubosp@7c8a6376-b613-0410-8ed7-c433467a09df>
2006-12-09 02:52:48 +0000

    git-svn-id: http://javaforge.com/svn/luntbuild/tags/luntbuild-1.3.5@107 7c8a6376-b613-0410-8ed7-c433467a09df

--- luntbuild/installer/scripts/configure-jdbc.bat
+++
@@ -1,1 +0,0 @@
-copy /Y "$INSTALL_PATH\web\WEB-INF\jdbc.$databaseType.properties" "$INSTALL_PATH\web\WEB-INF\jdbc.properties"
--- luntbuild/installer/scripts/configure-jdbc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cp -f "$INSTALL_PATH/web/WEB-INF/jdbc.$databaseType.properties" "$INSTALL_PATH/web/WEB-INF/jdbc.properties"
