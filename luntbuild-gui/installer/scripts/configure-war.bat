1cfbb9b24cdec7b13b9122d3a5d52caff315b705
lubosp <lubosp@7c8a6376-b613-0410-8ed7-c433467a09df>
2007-03-11 05:00:35 +0000

    Added support for standalone
    git-svn-id: http://javaforge.com/svn/luntbuild/trunk@148 7c8a6376-b613-0410-8ed7-c433467a09df

---
+++ luntbuild-gui/installer/scripts/configure-war.bat
@@ -0,0 +1,15 @@
+cd /D "$INSTALL_PATH\db"
+if "$luntbuildDemo" == "yes" (
+move demo\* .
+del /F luntbuild.script.base
+) else (
+move empty\* .
+)
+rmdir /S /Q demo empty
+cd /D "$INSTALL_PATH\web"
+jar -cvf "$INSTALL_PATH\luntbuild.war" .
+cd /D "$INSTALL_PATH"
+if "$appServerWebapps" == "" goto nodeploy
+rmdir /S /Q "$appServerWebapps\luntbuild"
+copy /Y "$INSTALL_PATH\luntbuild.war" "$appServerWebapps\luntbuild.war"
+:nodeploy
