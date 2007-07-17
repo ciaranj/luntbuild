60ed0128ac77a5a6553f2a427ac91d82e6499512
lubosp <lubosp@7c8a6376-b613-0410-8ed7-c433467a09df>
2007-07-17 21:14:50 +0000

    Added examle readme, classes ignored
    git-svn-id: http://javaforge.com/svn/luntbuild/trunk/luntbuild@208 7c8a6376-b613-0410-8ed7-c433467a09df

--- examples/src/java/com/luntsys/luntbuild/extensions/HelloWorldExtension.java
+++ examples/src/java/com/luntsys/luntbuild/extensions/HelloWorldExtension.java
@@ -3,6 +3,6 @@ package com.luntsys.luntbuild.extensions;
 public class HelloWorldExtension {
 
 	public void getHelloWorld() {
-		System.out.println("Hellow World! I am an extension");
+		System.out.println("Hello World! I am an extension!");
 	}
 }
