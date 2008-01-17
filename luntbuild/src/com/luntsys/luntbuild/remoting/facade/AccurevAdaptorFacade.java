ef373c13e676555be6b852d63ffe420ef4cd4b35
cleverclothe <cleverclothe@7c8a6376-b613-0410-8ed7-c433467a09df>
2008-01-17 05:00:16 +0000

    #6325 Modified Accurev fix to maintain backwards compatability.  Will need to revist for 2.0.
    git-svn-id: http://javaforge.com/svn/luntbuild/trunk@351 7c8a6376-b613-0410-8ed7-c433467a09df

--- luntbuild/src/com/luntsys/luntbuild/remoting/facade/AccurevAdaptorFacade.java
+++ luntbuild/src/com/luntsys/luntbuild/remoting/facade/AccurevAdaptorFacade.java
@@ -4,7 +4,6 @@
 
 package com.luntsys.luntbuild.remoting.facade;
 
-import com.luntsys.luntbuild.vcs.AccurevAdaptor;
 import com.luntsys.luntbuild.facades.lb12.VcsFacade;
 
 /**
@@ -13,12 +12,51 @@ import com.luntsys.luntbuild.facades.lb12.VcsFacade;
  * @author Jason Carreira <jcarreira@eplus.com>
  */
 public class AccurevAdaptorFacade extends VcsFacade {
+    private String user;
+    private String password;
+
     /**
      * Gets the corresponding VCS adaptor class name.
      *
      * @return the VCS adaptor class name
      */
     public String getVcsClassName() {
-        return AccurevAdaptor.class.getName();
+        return "com.luntsys.luntbuild.vcs.AccurevAdaptor";
+    }
+
+    /**
+     * Gets the password to login to the AccuRev server.
+     * 
+     * @return the password
+     */
+    public final String getPassword() {
+        return this.password;
+    }
+
+    /**
+     * Sets the password to login to the AccuRev server.
+     * 
+     * @param password the password
+     */
+    public final void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Gets the user to login to the AccuRev server.
+     * 
+     * @return the user
+     */
+    public final String getUser() {
+        return this.user;
+    }
+
+    /**
+     * Sets the user to login to the AccuRev server.
+     * 
+     * @param user the user
+     */
+    public final void setUser(String user) {
+        this.user = user;
     }
 }
