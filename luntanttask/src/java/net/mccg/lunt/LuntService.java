cfe64983616b946ae2ba0ad3f9e7e404aeccc05b
lubosp <lubosp@7c8a6376-b613-0410-8ed7-c433467a09df>
2008-02-10 23:51:11 +0000

    Moved to contrib/anttask
    git-svn-id: http://javaforge.com/svn/luntbuild/trunk@371 7c8a6376-b613-0410-8ed7-c433467a09df

--- luntanttask/src/java/net/mccg/lunt/LuntService.java
+++
@@ -1,5 +0,0 @@
-package net.mccg.lunt;
-
-public interface LuntService {
-	public String getArtifactUrl() throws Exception;
-}
--- luntanttask/test-src/java/net/mccg/lunt/LuntProjectFixture.java
+++
@@ -1,7 +0,0 @@
-package net.mccg.lunt;
-
-public class LuntProjectFixture {
-	public static LuntProject createBasicLuntProject() {
-		return new LuntProject("luntbuild", "luntPassword", "http://luntserver", "fakeProject", "fakeSchedule", "artifact.jar");
-	}
-}
