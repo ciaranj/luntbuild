d54c970130586aa7306f010db0beacf7ce90ae32
lubosp <lubosp@7c8a6376-b613-0410-8ed7-c433467a09df>
2008-10-13 16:26:05 +0000

    Initial project layout for Luntbuild2
    git-svn-id: http://javaforge.com/svn/luntbuild/trunk@446 7c8a6376-b613-0410-8ed7-c433467a09df

---
+++ luntbuild2/org.luntbuild.core/src/main/java/org/org/luntbuild/core/App.java
@@ -0,0 +1,13 @@
+package org.org.luntbuild.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
